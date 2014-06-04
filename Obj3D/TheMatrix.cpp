75d91cdcde748b7fa5497c9f632d6731d55d08b0
widarr <mangamaniac@gmx.at>
2014-06-04 15:48:47 +0200

    forgot some files - typical...

---
+++ Obj3D/TheMatrix.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   TheMatrix.cpp
+ * Author: vidar
+ * 
+ * Created on 03. Juni 2014, 16:24
+ */
+
+#include "TheMatrix.h"
+
+const glm::mat4 TheMatrix::IdentityMatrix = glm::mat4(1);
+
+TheMatrix::TheMatrix() {
+}
+
+TheMatrix::TheMatrix(const TheMatrix& orig) {
+}
+
+TheMatrix::~TheMatrix() {
+}
+
+Matrices* TheMatrix::getPipeline() {
+	  return &PipelineMatrices;
+}
+
+const glm::mat4 TheMatrix::getIdentityMatrix() {
+	  return IdentityMatrix;
+}
+
+void TheMatrix::setPipeline(Matrices m) {
+	  PipelineMatrices.M = m.M;
+	  PipelineMatrices.MVP = m.MVP;
+	  PipelineMatrices.P = m.P;
+	  PipelineMatrices.V = m.V;
+}
